ead6f02846e73991d6c22cb797a9ccb82a433975
Alexander <fedrennn@gmail.com>
2020-03-01 13:00:33 +0200

    Initial Commit

---
+++ HomeWork22APP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWork22APP
+//
+//  Created by Александр Федоров on 01.03.2020.
+//  Copyright © 2020 Alexander Fedorov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
